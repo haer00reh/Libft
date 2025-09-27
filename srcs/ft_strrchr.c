6857a70be9c48301e89600e257416d3fd8e3aa5a
haer00reh <haytamerrehyby@gmail.com>
2025-09-27 00:46:51 +0000

    OK

---
+++ srcs/ft_strrchr.c
@@ -0,0 +1,27 @@
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strrchr(char *str, int c)
+{
+	unsigned	char ch = (unsigned char)c;
+	int		i;
+
+	i = ft_strlen(str);
+
+	while (i >= 0)
+	{
+
+		if (str[i] == ch)
+			return (&str[i]);
+	i--;
+	}
+	return (0);
+}
