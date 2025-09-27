f26908eaff2f4900f5c54af4d8bd32013c758b79
haer00reh <haytamerrehyby@gmail.com>
2025-09-27 02:03:18 +0000

    OK

---
+++ srcs/ft_strstr.c
@@ -0,0 +1,19 @@
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	if (!*to_find)
+		return (str);
+	i = 0;
+	while (str[i])
+	{
+		j = 0;
+		while (to_find[j] && str[i + j] == to_find[j])
+			j++;
+		if (!to_find[j])
+			return (&str[i]);
+	i++;
+	}
+	return (0);
+}
