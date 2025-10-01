62d977fd64047507e90d5ba6730bb5fc2d1d80f7
haer00reh <haytamerrehyby@gmail.com>
2025-10-01 18:41:34 +0000

    OK

---
+++ srcs/ft_memalloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+
+void	ft_bzero(void *s, size_t n)
+{
+	size_t		i;
+	unsigned	char		*a = (unsigned char *)s;	
+
+	i = 0;
+	while (i < n)
+	{
+	a[i] = '\0';
+	i++;
+	}
+}
+
+void	*ft_memalloc(size_t size)
+{
+	void	*mem;
+
+	mem = malloc(size);
+	if (!mem)
+		return (NULL);
+	ft_bzero(mem, size);
+	return (mem);
+}
